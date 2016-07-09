802cb73835935809a6d918724fd1d7287a3cb1ad
Fokke Vermeulen <fokkevermeulen@gmail.com>
2016-07-09 16:44:37 +0200

    add routes

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ res.sendfile('public/index.html');
 });
 
 server.get('/new', function (req, res) {
-  res.sendfile('public/new.html');
+  res.json(
+    {
+       tournemant: 20,
+       uuid: '1232kjn3k4n343kj11234556'
+    }
+  );
 });
+
+server.post('/tournemant/:id/add_player', function(req, res) {
+  // TODO get name from player
+  // tournemant ID from routes
+  // response with success
+});
+
 
 server.listen(3000, function () {
   console.log('Example app listening on port 3000!');
